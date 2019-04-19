153021711ecf9927ae779dcfe5d3c28102ab0323
Vu <nguyentuanvu231198@gmail.com>
2019-04-19 21:46:24 +0700

    Update config.php

--- admincp/modules/config.php
+++ admincp/modules/config.php
@@ -4,7 +4,7 @@
 	$matkhau='';
 	$csdl='webtintuc';
 	$port=null;
-	$socket="/cloudsql/projectwebtintuc:us-central1:csdlwebtintuc";
+	$socket="/cloudsql/news-238114:us-central1:websitetintuc";
 	$connect=mysqli_connect($tenmaychu,$taikhoan,$matkhau,$csdl,$port,$socket);
 	mysqli_select_db($connect,$csdl);
 ?>
